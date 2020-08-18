afcd3bcec434823191c6368975fc989ebcce79ec
alpbintug <alp.bintug@gmail.com>
2020-08-18 14:52:10 +0300

    Creating GUI and adding buttons, just a checkpoint, nothing is complete

--- README.md
+++ README.md
@@ -2,8 +2,10 @@
 A screen recorder that I created to record my screen, because why pay for some random applications when you create one, right?
 
 ### TO-DO
-Create a GUI to help user start and stop recording
-Give user resolution options
-Give user FPS options
-Record computer audio synched with FPS and merge this with video file
-Create a GUI element to help user name the recorded video and specify save location
+'''
+- Create a GUI to help user start and stop recording
+- Give user resolution options
+- Give user FPS options
+- Record computer audio synched with FPS and merge this with video file
+- Create a GUI element to help user name the recorded video and specify save location
+'''
--- source.py
+++ source.py
@@ -1,10 +1,14 @@
 from cv2 import cv2
 import numpy as np
 import pyautogui
+import tkinter as tk
 
-def recordScreen(FPS,Length):
+keepRecording = False;
+frames = []
+
+def recordScreen(FPS):
     screenShots = []
-    for i in range (FPS*Length):
+    while keepRecording == True:
         img = pyautogui.screenshot()
         screenShots.append(img)
     return screenShots
@@ -21,19 +25,45 @@ def outputVideo(output, screenShots):
     for i in range(len(screenShots)):
         output.write(screenShots[i])
 
-
+def startRecording():
+    FPS = fpsSettings.get();
+    #window.iconify()
+    keepRecording = True;
+    #output = cv2.VideoWriter("output.avi",fourcc, FPS, (SCREEN_SIZE))
+    frames = recordScreen(FPS)
 
 SCREEN_SIZE = pyautogui.size()
 fourcc = cv2.VideoWriter_fourcc(*"XVID")
-FPS = 20
 videoLength = 1
-output = cv2.VideoWriter("output.avi",fourcc, FPS, (SCREEN_SIZE))
-recorded = recordScreen(FPS,videoLength)
-converted = convertScreenShots(recorded)
-outputVideo(output,converted)
+#converted = convertScreenShots(recorded)
+#outputVideo(output,converted)
 cv2.destroyAllWindows()
-output.release()
+#output.release()
+
+window = tk.Tk()
+window.geometry('300x300')
+window.title("Screen Recorder")
 
 
+labelFPSSetting = tk.Label(text = "Select FPS option")
+labelFPSSetting.place(x=90, y=40)
+
+
+fpsSettings = tk.ttk.Combobox(window)
+fpsSettings['values'] = (10, 15, 20, 25, 30, 45, 60)
+fpsSettings.current(4)
+fpsSettings.place(x=90, y=80,width = 115)
+
+
+btnStartRecording = tk.Button(window, text = "Start Recording",command = startRecording)
+btnStartRecording.place(x=90, y=120)
+
+btnStopRecording = tk.Button(window, text = "Stop Recording")
+btnStopRecording.place(x=90,y=160)
+btnStopRecording["state"] = "disabled"
+
+
+
+window.mainloop()
 
 
