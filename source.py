7ca298b2a1da5dd966d10fd9e770b398384d7c24
alpbintug <alp.bintug@gmail.com>
2020-08-18 12:50:56 +0300

    No longer using win32 api to get screen size which helps multiplatform support

--- source.py
+++ source.py
@@ -1,13 +1,13 @@
-import cv2
+from cv2 import cv2
 import numpy as np
 import pyautogui
-from win32api import GetSystemMetrics
 
-SCREEN_SIZE = (GetSystemMetrics(0),GetSystemMetrics(1))
+SCREEN_SIZE = pyautogui.size()
 fourcc = cv2.VideoWriter_fourcc(*"XVID")
-FPS = 20.0
+FPS = 20
+videoLength = 10
 output = cv2.VideoWriter("output.avi",fourcc, FPS, (SCREEN_SIZE))
-for i in range(200):
+for i in range(FPS*videoLength):
     img = pyautogui.screenshot()
     frame = np.array(img)
     frame = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
