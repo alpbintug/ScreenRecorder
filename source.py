b1d4ba3b2690f722b6e4d0b15a77bf51d1974868
alpbintug <alp.bintug@gmail.com>
2020-08-18 12:59:48 +0300

    Seperated code into functions, first taking screenshots, then converting them then outputting them in order to stop missing frames

--- source.py
+++ source.py
@@ -2,17 +2,35 @@ from cv2 import cv2
 import numpy as np
 import pyautogui
 
+def recordScreen(FPS,Length):
+    screenShots = []
+    for i in range (FPS*Length):
+        img = pyautogui.screenshot()
+        screenShots.append(img)
+    return screenShots
+
+def convertScreenShots(screenShots):
+    converted = []
+    for i in range(len(screenShots)):
+        frame = np.array(screenShots[i])
+        frame = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
+        converted.append(frame)
+    return converted
+
+def outputVideo(output, screenShots):
+    for i in range(len(screenShots)):
+        output.write(screenShots[i])
+
+
+
 SCREEN_SIZE = pyautogui.size()
 fourcc = cv2.VideoWriter_fourcc(*"XVID")
 FPS = 20
-videoLength = 10
+videoLength = 1
 output = cv2.VideoWriter("output.avi",fourcc, FPS, (SCREEN_SIZE))
-for i in range(FPS*videoLength):
-    img = pyautogui.screenshot()
-    frame = np.array(img)
-    frame = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
-    output.write(frame)
-
+recorded = recordScreen(FPS,videoLength)
+converted = convertScreenShots(recorded)
+outputVideo(output,converted)
 cv2.destroyAllWindows()
 output.release()
 
